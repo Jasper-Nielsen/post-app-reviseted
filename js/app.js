7b158663ef7597a8bc7ee0e647e635208a1e6789
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-05-02 11:25:43 +0200

    made files in modules

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-"use strict";
 
+import { getPosts, updatePost, deletePost, createPost } from "./rest-service.js";
+import {compareTitle, compareBody } from "./helpers.js"
 // const endpoint = "https://post-rest-api-default-rtdb.firebaseio.com";
-const endpoint = "";
+
 let posts;
 
 window.addEventListener("load", initApp);
@@ -26,17 +27,21 @@ function showCreatePostDialog() {
     document.querySelector("#dialog-create-post").showModal(); // show create dialog
 }
 
-function createPostClicked(event) {
+async function createPostClicked(event) {
     const form = event.target; // or "this"
     // extract the values from inputs from the form
     const title = form.title.value;
     const body = form.body.value;
     const image = form.image.value;
-    createPost(title, body, image); // use values to create a new post
+    const response = await createPost(title, body, image); // use values to create a new post
+    if (response.ok){
+        updatePostsGrid();
+    }
+    
     form.reset(); // reset the form (clears inputs)
 }
 
-function updatePostClicked(event) {
+async function updatePostClicked(event) {
     const form = event.target; // or "this"
     // extract the values from inputs in the form
     const title = form.title.value;
@@ -44,12 +49,21 @@ function updatePostClicked(event) {
     const image = form.image.value;
     // get id of the post to update - saved in data-id
     const id = form.getAttribute("data-id");
-    updatePost(id, title, body, image); // call updatePost with arguments
+    const response = await updatePost(id, title, body, image); // call updatePost with arguments
+    if(response.ok){
+        updatePostsGrid();
+    }
 }
 
-function deletePostClicked(event) {
+async function deletePostClicked(event) {
     const id = event.target.getAttribute("data-id"); // event.target is the delete form
-    deletePost(id); // call deletePost with id
+    const response = await deletePost(id); // call deletePost with id
+    if(response.ok){
+        updatePostsGrid();
+    }else{
+        console.log(response.status, response.statusText);
+        conseole.log("Something unspecified went wrong. Try again");
+    }
 }
 
 function deleteCancelClicked() {
@@ -81,13 +95,7 @@ async function updatePostsGrid() {
     showPosts(posts); // show all posts (append to the DOM) with posts as argument
 }
 
-// Get all posts - HTTP Method: GET
-async function getPosts() {
-    const response = await fetch(`${endpoint}/posts.json`); // fetch request, (GET)
-    const data = await response.json(); // parse JSON to JavaScript
-    const posts = prepareData(data); // convert object of object to array of objects
-    return posts; // return posts
-}
+
 
 function showPosts(listOfPosts) {
     document.querySelector("#posts").innerHTML = ""; // reset the content of section#posts
@@ -112,11 +120,14 @@ function showPost(postObject) {
     document.querySelector("#posts").insertAdjacentHTML("beforeend", html); // append html to the DOM - section#posts
 
     // add event listeners to .btn-delete and .btn-update
-    document.querySelector("#posts article:last-child .btn-delete").addEventListener("click", deleteClicked);
-    document.querySelector("#posts article:last-child .btn-update").addEventListener("click", updateClicked);
+    document.querySelector("#posts article:last-child .btn-delete").addEventListener("click",() => deleteClicked(postObject));
+    document.querySelector("#posts article:last-child .btn-update").addEventListener("click", () => updateClicked(postObject));
 
-    // called when delete button is clicked
-    function deleteClicked() {
+   
+}
+
+ // called when delete button is clicked
+    function deleteClicked(postObject) {
         // show title of post you want to delete
         document.querySelector("#dialog-delete-post-title").textContent = postObject.title;
         // set data-id attribute of post you want to delete (... to use when delete)
@@ -126,7 +137,7 @@ function showPost(postObject) {
     }
 
     // called when update button is clicked
-    function updateClicked() {
+    function updateClicked(postObject) {
         const updateForm = document.querySelector("#form-update-post"); // reference to update form in dialog
         updateForm.title.value = postObject.title; // set title input in update form from post title
         updateForm.body.value = postObject.body; // set body input in update form post body
@@ -134,84 +145,20 @@ function showPost(postObject) {
         updateForm.setAttribute("data-id", postObject.id); // set data-id attribute of post you want to update (... to use when update)
         document.querySelector("#dialog-update-post").showModal(); // show update modal
     }
-}
 
 function searchPosts(searchValue) {
     searchValue = searchValue.toLowerCase();
 
-    const results = posts.filter(checkTitle);
+   return posts.filter( post => post.title.toLowerCase().includes(searchValue));
 
-    function checkTitle(post) {
-        const title = post.title.toLowerCase();
-        return title.includes(searchValue);
-    }
+    // function checkTitle(post) {
+    //     const title = post.title.toLowerCase();
+    //     return title.includes(searchValue);
+    // }
 
-    return results;
+    // return results;
 }
 
-// Create a new post - HTTP Method: POST
-async function createPost(title, body, image) {
-    const newPost = { title, body, image }; // create new post object
-    const json = JSON.stringify(newPost); // convert the JS object to JSON string
-    // POST fetch request with JSON in the body
-    const response = await fetch(`${endpoint}/posts.json`, {
-        method: "POST",
-        body: json
-    });
-    // check if response is ok - if the response is successful
-    if (response.ok) {
-        console.log("New post succesfully added to Firebase 🔥");
-        updatePostsGrid(); // update the post grid to display all posts and the new post
-    }
-}
 
-// Update an existing post - HTTP Method: DELETE
-async function deletePost(id) {
-    const response = await fetch(`${endpoint}/posts/${id}.json`, {
-        method: "DELETE"
-    });
-    if (response.ok) {
-        console.log("New post succesfully deleted from Firebase 🔥");
-        updatePostsGrid(); // update the post grid to display all posts and the new post
-    }
-}
 
-// Delete an existing post - HTTP Method: PUT
-async function updatePost(id, title, body, image) {
-    const postToUpdate = { title, body, image }; // post update to update
-    const json = JSON.stringify(postToUpdate); // convert the JS object to JSON string
-    // PUT fetch request with JSON in the body. Calls the specific element in resource
-    const response = await fetch(`${endpoint}/posts/${id}.json`, {
-        method: "PUT",
-        body: json
-    });
-    // check if response is ok - if the response is successful
 
-    if (response.ok) {
-        console.log("Post succesfully updated in Firebase 🔥");
-        updatePostsGrid(); // update the post grid to display all posts and the new post
-    }
-}
-
-// ============== helper function ============== //
-
-// convert object of objects til an array of objects
-function prepareData(dataObject) {
-    const array = []; // define empty array
-    // loop through every key in dataObject
-    // the value of every key is an object
-    for (const key in dataObject) {
-        const object = dataObject[key]; // define object
-        object.id = key; // add the key in the prop id
-        array.push(object); // add the object to array
-    }
-    return array; // return array back to "the caller"
-}
-
-function compareTitle(post1, post2) {
-    return post1.title.localeCompare(post2.title);
-}
-
-function compareBody(post1, post2) {
-    return post1.body.localeCompare(post2.body);
-}
---
+++ js/rest-service.js
@@ -0,0 +1,52 @@
+import { prepareData } from "./helpers.js";
+const endpoint =
+  "https://test-d2238-default-rtdb.europe-west1.firebasedatabase.app/";
+
+// Create a new post - HTTP Method: POST
+async function createPost(title, body, image) {
+  const newPost = { title, body, image }; // create new post object
+  const json = JSON.stringify(newPost); // convert the JS object to JSON string
+  // POST fetch request with JSON in the body
+  const response = await fetch(`${endpoint}/posts.json`, {
+    method: "POST",
+    body: json,
+  });
+  
+  
+  return response;
+  
+
+}
+
+// Update an existing post - HTTP Method: DELETE
+async function deletePost(id) {
+  const response = await fetch(`${endpoint}/posts/${id}.json`, {
+    method: "DELETE",
+  });
+  
+   return response;
+}
+
+// Delete an existing post - HTTP Method: PUT
+async function updatePost(id, title, body, image) {
+  const postToUpdate = { title, body, image }; // post update to update
+  const json = JSON.stringify(postToUpdate); // convert the JS object to JSON string
+  // PUT fetch request with JSON in the body. Calls the specific element in resource
+  const response = await fetch(`${endpoint}/posts/${id}.json`, {
+    method: "PUT",
+    body: json,
+  });
+  // check if response is ok - if the response is successful
+
+   return response;
+}
+
+// Get all posts - HTTP Method: GET
+async function getPosts() {
+  const response = await fetch(`${endpoint}/posts.json`); // fetch request, (GET)
+  const data = await response.json(); // parse JSON to JavaScript
+  const posts = prepareData(data); // convert object of object to array of objects
+  return posts; // return posts
+}
+
+export { getPosts, updatePost, deletePost, createPost };
